9117249a6ebbaf5c0b13787826f767fdf818f367
tdhung03 <mspotify208@gmail.com>
2023-05-24 14:56:29 +0700

    Init purchase feature

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from purchase.models import *
 
 # Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartDetail)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from products.models import *
+from users.models import *
+
 
 # Create your models here.
+
+class Cart(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="CartDetails")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.IntegerField()
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="OrderDetails")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    sub_price = models.IntegerField()
