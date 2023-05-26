143c030906b5dc7cf8fbc0061d0536d9cd6201cb
tdhung03 <mspotify208@gmail.com>
2023-05-26 17:15:53 +0700

    modify profile model

--- users/models.py
+++ users/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, blank=True, null=True)
-    email = models.EmailField(max_length=500, blank=True, null=True)
-    username = models.CharField(max_length=200, blank=True, null=True)
-    phone_number = models.CharField(max_length=200, blank=True, null=True)
-    address = models.CharField(max_length=200, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=500)
+    username = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
 
     def __str__(self):
         return self.username
