001daf5b1b6d1099877dbfa41961a68711fcdfa7
tdhung03 <mspotify208@gmail.com>
2023-05-31 21:22:28 +0700

    config cloudinary

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +133,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Cloudinary configuration
+cloudinary.config(
+    cloud_name='',
+    api_key='',
+    api_secret=''
+)
