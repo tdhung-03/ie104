7565217879fdc94d5eb380989384d9377eebee09
tdhung03 <mspotify208@gmail.com>
2023-06-01 16:10:59 +0700

    levenshtein for search

--- products/api/views.py
+++ products/api/views.py
@@ -1,11 +1,24 @@
 from products.api.serializers import *
 from products.models import *
 from rest_framework import generics
+from django.db.models.functions import Lower
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 class ProductList(generics.ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializerForProductList
+
+    def get_queryset(self):
+        search_query = self.request.query_params.get('name', None)
+        queryset = Product.objects.all()
+
+        if search_query:
+            # Perform Levenshtein search using trigram similarity
+            queryset = queryset.annotate(
+                similarity=TrigramSimilarity(Lower('name'), search_query),
+            ).filter(similarity__gt=0.3).order_by('-similarity')
+
+        return queryset
 
 
 class ProductDetail(generics.RetrieveAPIView):
